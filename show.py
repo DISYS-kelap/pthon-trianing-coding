2bb2881d4b0f652a488c280bc65eaafbd115213b
disys-caleb <95436262+DISYS-kelap@users.noreply.github.com>
2021-12-19 00:30:45 -0800

    Add files via upload

---
+++ show.py
@@ -0,0 +1,14 @@
+import pack
+
+class show(pack.career):
+    def __init__(self,w):
+        print("THE Movie")
+        if ( w is not==list):
+            print("this movie is not present')
+
+        else:
+            print("try again")
+            
+        super().__init__("School", 9)
+        self.now = w
+sb=show(10)
